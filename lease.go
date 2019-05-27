26310d65bba3cc2f4b806e4dbd52fb4136880969
npotts <npotts@users.noreply.github.com>
2019-05-27 14:01:35 -0600

    Add JSON tags

--- lease.go
+++ lease.go
@@ -29,47 +29,48 @@ A Lease contains the data specified in the following format:
 */
 type Lease struct {
 	//IP address given to the lease
-	IP net.IP
+	IP net.IP `json:"ip"`
 
 	//Start time of the lease
-	Starts time.Time
+	Starts time.Time `json:"starts"`
 
 	//Time when the lease expires
-	Ends time.Time
+	Ends time.Time `json:"ends"`
 
 	//Tstp is specified if the failover protocol is being used, and indicates what time the peer has been told the lease expires.
-	Tstp time.Time
+	Tstp time.Time `json:"tstp"`
 
 	//Tsfp is also specified if the failover protocol is being used, and indicates the lease expiry time that the peer has acknowledged
-	Tsfp time.Time
+	Tsfp time.Time `json:"tsfp"`
 
 	//Atsfp is the actual time sent from the failover partner
-	Atsfp time.Time
+	Atsfp time.Time `json:"atsfp"`
 
 	//Cltt is the client's last transaction time
-	Cltt time.Time
+	Cltt time.Time `json:"cllt"`
 
 	/*The binding state statement declares the lease's binding state. When the DHCP server is
 	not configured to use the failover protocol, a lease's binding state will be either
 	active or free. The failover protocol adds some additional transitional states, as
 	well as the backup state, which indicates that the lease is available for allocation
 	by the failover secondary.*/
-	BindingState string
+	BindingState string `json:"binding-state"`
 
 	//The next binding state statement indicates what state the lease will move to when the current state expires. The time when the current state expires is specified in the ends statement.
-	NextBindingState string
+	NextBindingState string `json:"next-binding-state"`
 
 	//The hardware statement records the MAC address of the network interface on which the lease will be used. It is specified as a series of hexadecimal octets, separated by colons.
 	Hardware struct {
-		Hardware string
-		MAC      net.HardwareAddr
-	}
+		Hardware string           `json:"hardware"`
+		MAC      string           `json:"mac"`
+		MACAddr  net.HardwareAddr `json:"-"`
+	} `json:"hardware"`
 
 	//The uid statement records the client identifier used by the client to acquire the lease. Clients are not required to send client identifiers, and this statement only appears if the client did in fact send one. Client identifiers are normally an ARP type (1 for ethernet) followed by the MAC address, just like in the hardware statement, but this is not required.
-	UID string
+	UID string `json:"uid"`
 
 	//Clients provided hostname
-	ClientHostname string
+	ClientHostname string `json:"client-hostname"`
 }
 
 var (
@@ -89,8 +90,9 @@ var (
 		regexp.MustCompile("hardware (?P<D>.*);"): func(l *Lease, line string) {
 			s := strings.SplitN(line, " ", 2)
 			l.Hardware.Hardware = s[0]
+			l.Hardware.MAC = s[1]
 			if m, e := net.ParseMAC(s[1]); e == nil {
-				l.Hardware.MAC = m
+				l.Hardware.MACAddr = m
 			}
 		},
 	}
